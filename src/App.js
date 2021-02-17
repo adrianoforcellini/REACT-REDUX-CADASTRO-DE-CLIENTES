f805c3040cf0bd078d24a7c99aa1f5a0bdd9e86f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-02-17 20:02:03 -0300

    Paginas criadas + Login.css - implementar funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Clientes from './pages/Clientes';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/404';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                a
-            </div>
-        );
-    }
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={ Login } />
+        <Route path="/clientes" component={ Clientes } />
+        <Route exact path="/" component={ Home } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
